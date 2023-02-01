49273091081cf7927e4421dc64e709a8ab1a1440
oooo91 <hyelneu29@gmail.com>
2023-02-01 21:08:08 +0900

    sub test

---
+++ src/test/java/com/example/sub_test/SubTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sub_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
